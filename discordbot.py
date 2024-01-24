0601fe2a88c28ffc618850fd30b355d30c5927de
Y. Ozaki <62942499+mttk1528@users.noreply.github.com>
2024-01-24 15:11:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -21,25 +21,7 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if message.content.startswith('!gpt'):
-        print(message.content)
-        print(message.content.split('>')[1].lstrip())
-        messages.append({"role": "user", "content": message.content.split('>')[1].lstrip()})
 
-        openai_api_key = getenv('OPENAI_API_TOKEN')
-        openai.api_key = openai_api_key
-
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages
-        )
-
-        print(completion.choices[0].message.content)
-        await message.channel.send(completion.choices[0].message.content)
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
