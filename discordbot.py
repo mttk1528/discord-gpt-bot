2b68a7308480a9d1879a94ac859dda1f1cf3cf54
Y. Ozaki <62942499+mttk1528@users.noreply.github.com>
2024-01-24 15:02:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,26 +1,45 @@
 import discord
+import openai
 import traceback
 from discord.ext import commands
 from os import getenv
 
-intents = discord.Intents.default()
-intents.message_content = True
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+class ChatGPT:
+    def __init__(self, system_setting):
+        self.system = {"role": "system", "content": system_setting}
+        self.input_list = [self.system]
+        self.logs = []
+
+    def input_message(self, input_text):
+        self.input_list.append({"role": "user", "content": input_text})
+        result = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=self.input_list
+        )
+        self.logs.append(result)
+        self.input_list.append(
+            {"role": "assistant", "content": result.choices[0].message.content}
+        )
+        
+@client.event
+async def on_ready():
+    print("activated")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('!gpt'):
+        question = message.content[4:]
+        api = ChatGPT(system_setting="You should act as an assistant. Conversation starting now")
+        api.input_message(question)
+        answer = api.input_list[-1]["content"]
+        await message.channel.send(answer)
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
+openai.api_key = getenv('OPENAI_API_TOKEN')
 token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
+client.run(token)
 
